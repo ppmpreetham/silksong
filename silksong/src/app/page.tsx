a243c55766987354eed000777421254a973681f2
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-09-30 13:27:02 +0530

    add: bg

--- silksong/src/app/page.tsx
+++ silksong/src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-black text-white">
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center text-white">
       <Header />
     </div>
   );
--- silksong/src/components/Header.tsx
+++ silksong/src/components/Header.tsx
@@ -4,6 +4,7 @@ import React, {useRef} from 'react'
 import Image from 'next/image'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import "./Header.css"
 
 const Header = () => {
   const container = useRef<HTMLDivElement>(null)
@@ -13,16 +14,40 @@ const Header = () => {
         if (container.current) {
             gsap.fromTo(
                 ImageRef.current,
-                { filter: "blur(500px)" },
-                { filter: "blur(0px)", duration: 3, ease: "power2.out" }
+                { filter: "blur(100px)" },
+                { filter: "blur(0px)", duration: 2, ease: "power2.out" }
             );
         }
     }, { scope: container });
     
     return (
-    <div ref={container} className="flex flex-col items-center justify-center">
-      <Image src="/silksong.png" alt="Silksong Logo" width={300} height={300} ref={ImageRef}/>
-    </div>
+    <>
+      <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div ref={container} className="flex flex-col items-center justify-center relative z-10">
+        <Image src="/silksong.png" alt="Silksong Logo" width={500} height={500} ref={ImageRef}/>
+      </div>
+    </>
   )
 }
 
