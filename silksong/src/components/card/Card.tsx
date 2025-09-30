b5bebbd4e74a394cce1d9146e19712eb0301131a
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-09-30 16:28:07 +0530

    fix: card back

--- silksong/src/components/card/Card.tsx
+++ silksong/src/components/card/Card.tsx
@@ -1,17 +1,22 @@
-import React, { useRef, useState } from 'react'
+import { useTexture } from '@react-three/drei/core/Texture';
+import React from 'react'
 import * as THREE from 'three'
 
-const CardMesh = () => {
-  const ref = useRef<THREE.Mesh>(null)
-
+const CardMesh = ({ imageUrl, ref }: {imageUrl: string; ref: React.Ref<THREE.Mesh>;}) => {
+  const texture = useTexture(imageUrl);
+  const backside = useTexture('/cards/card1.jpg');
   return (
     <mesh ref={ref}>
-      <boxGeometry args={[5, 10, 1]} scale={10}/>
-      <meshStandardMaterial color={'white'} />
+      {/* Front side */}
+      <planeGeometry args={[8, 12]} />
+      <meshStandardMaterial color="white" map={texture} alphaMap={texture} />
+      {/* Back side */}
+      <mesh position={[0, 0, -0.01]}>
+      <planeGeometry args={[8, 12]} />
+      <meshStandardMaterial color="white" map={backside} />
+      </mesh>
     </mesh>
   )
 }
-
-
 
 export default CardMesh
--- silksong/src/components/card/Env.tsx
+++ silksong/src/components/card/Env.tsx
@@ -1,18 +1,24 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import React, { useRef } from 'react'
+
 import CardMesh from './Card'
 import * as THREE from 'three'
 
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+
 const Env = () => {
+  const card1Ref = useRef<THREE.Mesh>(null);
   return (
     <div className="h-screen w-screen">
-        <Canvas className='' camera={{ position: [0, 0, 20], fov: 50 }}>
-          <ambientLight intensity={10}/>
-          <CardMesh />
-        </Canvas>
+      <Canvas camera={{ position: [0, 0, 20], fov: 50 }}>
+        <ambientLight intensity={1} />
+        <CardMesh imageUrl="/cards/card2.jpg" ref={card1Ref} />
+      </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default Env
+export default Env;
